0cd77c00183c9e53068b630107d6c4f2efdbb2ab
hipopotamus <qkqntjddn@gmail.com>
2021-02-22 16:04:35 +0900

    c00_ex08

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -14,14 +14,11 @@ int power(int a, int b)
 	return (a);
 }
 
-void convert(int n, int size)
+void convert(int n, int size, char buff[])
 {
-	char buff[size];
 	int i;
 
 	i = -1;
-	if (n == 0)
-		return ;
 	while(++i <= size - 1)
 	{
 		if (n / power(10, size -1 - i) < 1)
@@ -30,40 +27,41 @@ void convert(int n, int size)
 			buff[i] = '0' + n / power(10, size - 1 - i);
 		n = n % power(10, size - 1 - i);
 	}
-	write(1, buff, size);
 }
 
-int check(int n, int size)
+int check(char buff[], int size)
 {
 	int i;
 
 	i = -1;
 	while(++i < size - 1)
 	{
-		if(n / power(10 , size - 1 - i) > n % power(10 , size - 1 - i) / power(10 , size - 2 - i))
-			return (2);
-		else
-			return (3);
-		n = n % power(10, size - 1 - i);
+		if(buff[i] >= buff[i+1])
+			return(0);
 	}
+	return(1);
 }
 
 void ft_print_combn(int n)
 {
 	int i;
+	char buff[n];
+	char end = '0' + 10 - n;
 	
 	i = 0;
-	while(++i < power(10, n) - 1)
+	while(i++ < power(10 , n) - 1)
 	{
-		if (check(i, n) == 3)
+		convert(i, n, buff);
+		if (check(buff, n) == 1)
 		{
-			convert(i,n);
-			write(1, ", ", 2);
+			write(1, buff, n);
+			if (buff[0] != end)
+				write(1, ", ", 2);
 		}
 	}
 }
 
 int main()
 {
-	ft_print_combn(3);
+	ft_print_combn(2);
 }
